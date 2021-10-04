6cba2cddc25af99e573960ce3576cf6865b4c747
Zach-EE <zachholloway.n@gmail.com>
2021-10-04 17:25:25 -0400

    added Experience.js TODO:add description to experience <p>

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Experience from "./components/Experience";
 import "./App.css";
 
 function App() {
   return (
     <body id="page-top">
       <Navbar />
-      <div className="container-fluid p-0"><About /></div>
+      <div className="container-fluid p-0">
+        <About />
+        <Experience />
+      </div>
     </body>
   );
 }
