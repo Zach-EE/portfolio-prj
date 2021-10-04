f5b21a38a0d6c51d720e07bb32aac66e3db9144d
Zach-EE <zachholloway.n@gmail.com>
2021-10-04 16:32:38 -0400

    added About.js TODO: add icons to page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 import "./App.css";
 
 function App() {
   return (
     <body id="page-top">
       <Navbar />
-      <div className="container-fluid p-0"></div>
+      <div className="container-fluid p-0"><About /></div>
     </body>
   );
 }
