dfbbf78714f2689dd1904a0d3cd4404f0737c78b
Zach-EE <zachholloway.n@gmail.com>
2021-10-05 13:32:42 -0400

    added Skills.js component to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
+import Skills from "./components/Skills";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <About />
         <Experience />
         <Education />
+        <Skills />
       </div>
     </body>
   );
