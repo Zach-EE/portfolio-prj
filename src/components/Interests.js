be28d440fd57436f73264add876219949e734051
Zach-EE <zachholloway.n@gmail.com>
2021-10-05 15:59:21 -0400

    finished adding all .js components

---
+++ src/components/Interests.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Interests = () => {
+  return (
+    <section class="resume-section" id="interests">
+      <div class="resume-section-content">
+        <h2 class="mb-5">Interests</h2>
+        <p>
+          Apart from being an engineer and software developer, I enjoy researching and participating in fin-tech and decentralized finance. This interest has branched out into budding passion for technical and fundamental analysis of multiple financial markets. Leveraging my robust background and skills to beat the overall market has become a favorite hobby of mine.
+        </p>
+        <p class="mb-0">
+          I also continue to use my knowledge in electrical engineering to tinker and build DIY robotics and automation projects. Currently my home workshop hosts an assortment of devices ranging from test equipment like oscilloscopes and soldering irons to development hardware like FPGAs and Raspberry Pis. Do not hesitate to reach out with any volunteer opportunities or project assistance!
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Interests;
